3301bcfb4a0438e267e2838563495bdd563a03bf
OoogleBoogle <simattfield@gmail.com>
2016-07-08 10:47:36 -0700

    Updated a few bits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,11 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
-import { ROUTER_DIRECTIVES, Router } from '@angular/router';
-import { AngularFire, AngularFireAuth, FirebaseAuthState } from 'angularfire2'
+import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 import { NavbarComponent } from './shared';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css'],
   directives: [ROUTER_DIRECTIVES, NavbarComponent]
 })
-export class AppComponent {   
-  constructor(
-    public af: AngularFire,
-    private router: Router) { }
-
-}
+export class AppComponent {}
